2d8195e64d365129ef6fe4e5696ee0444b9a4c79
krkbheet@gmail.com <krkbheet@gmail.com>
2025-11-30 20:54:48 +0100

    fix seo: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,10 @@ import Navbar from '@/components/ui/Navbar'
 
 export const metadata: Metadata = {
   title: 'Zelana',
-  description: 'A privacy focused ZK rollup on Solana',
+  description: 'A privacy focused ZK rollup on Solana. Exploring solutions that enchance privacy from the ground up.',
+  keywords: [
+    "Solana", "Solana zk rollup", "Solana rollup", "rollup", "zk rollup", "L2", "Solana L2", "zero-knowledge"
+  ],
   openGraph: {
     title: 'Zelana - Next Generation of Privacy',
     description: 'A privacy focused ZK rollup on Solana',
@@ -36,11 +39,40 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const orgJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "Zelana",
+    "url": "https://zelana.org",
+    "logo": "https://zelana.org/og-metadata-image.png",
+    "description": "A privacy-focused ZK rollup on Solana, enhancing privacy from the ground up.",
+    "sameAs": [
+      "https://twitter.com/zelanalabs",
+      "https://github.com/zelana-labs"
+    ]
+  };
+
+  const websiteJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": "Zelana",
+    "url": "https://zelana.org",
+  };
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(orgJsonLd) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteJsonLd) }}
+        />
+      </head>
       <body>
         <Providers>
-          <Navbar/>
+          <Navbar />
           {children}
         </Providers>
       </body>
